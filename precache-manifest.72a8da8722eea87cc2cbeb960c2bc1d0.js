eae031445b99782aad67abde0a3daabf77fe5b81
el_zorich <elenazorich@gmail.com>
2020-03-18 17:07:34 +0100

    Initial commit

---
+++ precache-manifest.72a8da8722eea87cc2cbeb960c2bc1d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b0dae6bfae4e9ecd1f46ed9c9e2fa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a786198b7fe2717d9141",
+    "url": "/static/css/main.cc4572f2.chunk.css"
+  },
+  {
+    "revision": "245c8872d4713f637e91",
+    "url": "/static/js/2.9abeffea.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/static/js/2.9abeffea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a786198b7fe2717d9141",
+    "url": "/static/js/main.fd870ddd.chunk.js"
+  },
+  {
+    "revision": "0b0aa47db8a297d08cd9",
+    "url": "/static/js/runtime-main.0d5a3dd1.js"
+  }
+]);
